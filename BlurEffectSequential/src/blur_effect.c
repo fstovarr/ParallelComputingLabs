c00cbc5b91513fcffd66625149880e7d7ce6d7dd
Fabio Tovar <fstovarr@unal.edu.co>
2020-03-31 01:29:08 -0500

    Good approach

--- BlurEffectSequential/src/blur_effect.c
+++ BlurEffectSequential/src/blur_effect.c
@@ -6,59 +6,92 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "../lib/stb/stb_image_write.h"
 
-#define DIR_IMG_INPUT "img/favicon.png"
-// #define DIR_IMG_INPUT "img/test.png"
+// #define DIR_IMG_INPUT "img/favicon.png"
+#define DIR_IMG_INPUT "img/test.png"
+// #define DIR_IMG_INPUT "img/test2.jpg"
 #define DIR_IMG_OUTPUT "out/out_test.png"
 
-const int kernel_pad = 2;
-const double kernel[5][5] = { 
-    {1.0 / 256,  4.0 / 256,  6.0 / 256,  4.0 / 256, 1.0 / 256}, 
-    {4.0 / 256, 16.0 / 256, 24.0 / 256, 16.0 / 256, 4.0 / 256}, 
-    {6.0 / 256, 24.0 / 256, 36.0 / 256, 24.0 / 256, 6.0 / 256}, 
-    {4.0 / 256, 16.0 / 256, 24.0 / 256, 16.0 / 256, 4.0 / 256}, 
-    {1.0 / 256,  4.0 / 256,  6.0 / 256,  4.0 / 256, 1.0 / 256} 
-};
+void generateGaussianKernel(double* k, int size) {
+    double sigma = 1.0;
+    double two_sigma_sq = 2 * sigma * sigma;
 
-// const int kernel_pad = 1;
-// const double kernel[3][3] = {
-//     { 0.0625,   0.125,  0.0625 },
-//     { 0.1250,   0.25,  0.125 },
-//     { 0.0625,   0.125,  0.0625 }
-// };
+    double sum = 0.0;
+    double res = 0.0;
+    int mid_size = size / 2;
 
-void calculatePixel(unsigned char *in, unsigned char *out, int i, int w, int h, int channels) {
+    for (int x = -mid_size; x <= mid_size; x++) 
+        for (int y = -mid_size; y <= mid_size; y++) {
+            int idx = (x + mid_size) * size + y + mid_size;
+            double r = sqrt(x * x + y * y);
+            res = (double)((1 / (two_sigma_sq * M_PI)) * exp(-r * r / two_sigma_sq));
+            memcpy(k + idx, &res, sizeof(res));
+            sum += *(k + idx);
+            // printf("(%f %d)", *(k + idx), idx);
+        }
+        // printf("\n");
+
+    // printf("SUM %f \n", sum);
+
+    for (int x = 0; x < size; x++)
+        for (int y = 0; y < size; y++) {
+            res = *(k + x * size + y) / sum;
+            memcpy(k + x * size + y, &res, sizeof(res));
+        }
+}
+
+void calculatePixel(unsigned char *in, unsigned char *out, int i, int w, int h, int channels, double* kernel, int kernel_size) {
+    int kernel_pad = kernel_size / 2;
+
     for (int l = 0; l < channels; l++) {
         double total = 0.0;
-        for (int m = -kernel_pad/2; m <= kernel_pad; m++)
-            for (int n = -kernel_pad/2; n <= kernel_pad; n++)
-                total += kernel[m + kernel_pad][n + kernel_pad] * in[(i + l) + m * channels + n * channels];
+        for (int m = -kernel_pad; m <= kernel_pad; m++)
+            for (int n = -kernel_pad; n <= kernel_pad; n++) {
+                double v = *(kernel + (m  + kernel_pad) * kernel_size + (n + kernel_pad));
+                total += v * in[(i + l) + (m + kernel_pad) * channels + (n + kernel_pad) * channels];
+            }
+
         out[i + l] = total;
     }
 }
 
-void applyFilter(unsigned char *in, unsigned char *out, int w, int h, int c) {
+void applyFilter(unsigned char *in, unsigned char *out, int w, int h, int c, double* kernel, int kernel_size) {
+    int kernel_pad = kernel_size / 2;
     size_t size = w * h * c;
+
     for (int i = 0; i < size; i += c)
         if(i > kernel_pad * w * c && // Top
             i < (size - kernel_pad * w * c) && // Bottom
             i % (w * c) >= kernel_pad * c && // Left
             i % (w * c) < (w * c - kernel_pad * c)) // Right
-            calculatePixel(in, out, i, w, h, c);
+            calculatePixel(in, out, i, w, h, c, kernel, kernel_size);
         else
             for (int j = 0; j < c; j++)
                 out[i + j] = 0;
 }
 
 int main() {
+    int KERNEL_SIZE = 3;
+    double kernel[KERNEL_SIZE][KERNEL_SIZE];
+    generateGaussianKernel(kernel, KERNEL_SIZE);
+
+    // Print kernel
+
+    // for (int i = 0; i < KERNEL_SIZE; i++) {
+    //     for (int j = 0; j < KERNEL_SIZE; j++)
+    //         printf("%f ", t[i][j]);
+    //     printf("\n");
+    // }
+
+    // printf("\n");
+
     int width, height, channels;
-    unsigned char *data = stbi_load(DIR_IMG_INPUT, &width, &height, &channels, 0);
-    // channels = 3;
+    unsigned char *data = stbi_load(DIR_IMG_INPUT, &width, &height, &channels, STBI_default);
 
     if (data != NULL) {
         printf("Image dimensions: (%dpx, %dpx) and %d channels.\n", width, height, channels);
         
         unsigned char *output_image = malloc(width * height * channels);
-        applyFilter(data, output_image, width, height, channels);
+        applyFilter(data, output_image, width, height, channels, kernel, KERNEL_SIZE);
 
         stbi_write_png(DIR_IMG_OUTPUT, width, height, channels, output_image, width * channels);
     } else {
