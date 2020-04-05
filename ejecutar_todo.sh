087df2a531b6620f96432d4170787f3b206ba02f
Edwin M <ermahechap@unal.edu.co>
2020-04-05 13:43:05 -0500

    Testing ready

---
+++ ejecutar_todo.sh
@@ -0,0 +1,24 @@
+# Run like ./ejecutar_todo.sh IMG_PATH
+
+pip install --user matplotlib
+pip install --user numpy
+pip install --user pandas
+IMG_PATH=$1
+# I make all because why not
+make -C BlurEffect
+make -C BlurEffectPool
+make -C BlurEffectSequential
+make -C BlurEffectBlock
+
+# Runs Sequential
+echo "------- SEQUENTIAL------"
+./test-bench-sequential.sh $IMG_PATH Results/sequential_data.out
+# Runs Block data
+echo "--------- BLOCK --------"
+./test-bench.sh BlurEffectBlock/bin/blur-effect $IMG_PATH BlurEffectBlock/out Results/block.out
+# Runs cyclic data
+echo "--------- CYCLIC--------"
+./test-bench.sh BlurEffect/bin/blur-effect $IMG_PATH BlurEffect/out Results/cyclic_data.out 
+# Runs pool
+echo "---------- POOL ---------"
+./test-bench.sh BlurEffectPool/bin/blur-effect $IMG_PATH BlurEffectPool/out Results/pool_data.out 
